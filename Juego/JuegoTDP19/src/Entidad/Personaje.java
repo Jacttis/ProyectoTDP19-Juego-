c7062e2d63480057b798eb9e647a6d70daf54f73
AxelK12 <51532945+AxelK12@users.noreply.github.com>
2019-09-02 18:14:48 -0300

    Documentacion personaje

--- Juego/JuegoTDP19/src/Entidad/Personaje.java
+++ Juego/JuegoTDP19/src/Entidad/Personaje.java
@@ -1,36 +1,55 @@
 package Entidad;
+
 import java.awt.*;
 
 import Inteligencia.Inteligencia;
 
 public abstract class Personaje extends Entidad {
-	
-	protected int vida,damage;
+
+	protected int vida, damage;
 	protected float velocidadAtaque;
 
-	
-	//protected Premio objetos [];
-	
+	// protected Premio objetos [];
 
-	public Personaje(Point pos,int vida,int damage,float velocidadAtaque) {
+	/**
+	 * Crea el personaje
+	 * 
+	 * @param pos
+	 * @param vida
+	 * @param damage
+	 * @param velocidadAtaque
+	 */
+
+	public Personaje(Point pos, int vida, int damage, float velocidadAtaque) {
 		super(pos);
-		this.vida=vida;
-		this.damage=damage;
-		this.velocidadAtaque=velocidadAtaque;
-		this.IA=IA;
+		this.vida = vida;
+		this.damage = damage;
+		this.velocidadAtaque = velocidadAtaque;
+		
 	}
-	//Getters
+	// Getters
+
+	/**
+	 * @return Retorna la vida del personaje
+	 */
 	public int getVida() {
 		return vida;
 	}
 
+	/**
+	 * 
+	 * @return Retorna el damage del personaje
+	 */
 	public int getDamage() {
 		return damage;
 
 	}
+	/**
+	 * 
+	 * @return retorn la velocidad de ataque del personaje
+	 */
 	public float getVelocidadAtaque() {
 		return velocidadAtaque;
 	}
-	
-	
+
 }
