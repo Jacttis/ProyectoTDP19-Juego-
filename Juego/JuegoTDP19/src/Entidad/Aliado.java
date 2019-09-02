9f3b10f5f16dddf9a14aa1fe94d43503fc1cfba5
Julian <julianacttis@hotmail.com>
2019-09-02 17:26:47 -0300

    Aliado

--- Juego/JuegoTDP19/src/Entidad/Aliado.java
+++ Juego/JuegoTDP19/src/Entidad/Aliado.java
@@ -9,15 +9,25 @@ import java.awt.*;
 
 public class Aliado extends Personaje {
 
-    String descripcion;
+     protected String descripcion;
 
-    public Aliado(,Point pos, int vida, int damage, int width, int height, float velocidadAtaque, Inteligencia IA,String Descripcion) {
+    /**
+     * Crea un Personaje Aliado
+     * @param pos
+     * @param vida
+     * @param damage
+     * @param width
+     * @param height
+     * @param velocidadAtaque
+     * @param IA
+     * @param Descripcion
+     */
+    public Aliado(Point pos, int vida, int damage,float velocidadAtaque, Inteligencia IA,String Descripcion) {
         super(pos, vida, damage, width, height, velocidadAtaque, IA);
         image =new ImagenIcon[2];
         descripcion=Descripcion;
 
     }
-
     /**
      * Devuelve la descripcion del Personaje Aliado
      * @return String con personaje aliado
