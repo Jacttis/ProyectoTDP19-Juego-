ab1f4a3fc1b9395eb4c2aaf2274cef06ccb5b4e8
Julian <julianacttis@hotmail.com>
2019-09-02 17:19:39 -0300

    Aliado Cambios

--- Juego/JuegoTDP19/src/Entidad/Aliado.java
+++ Juego/JuegoTDP19/src/Entidad/Aliado.java
@@ -1,13 +1,30 @@
-package  Entidades;
+/**
+ * Clase creada por Julian Acttis,Axel Fontana, Leandro Moran
+ */
+package  Entidad;
 
 import Inteligencia.Inteligencia;
 
 import java.awt.*;
 
-public class Aliado extends Entidades.Personaje {
+public class Aliado extends Personaje {
 
+    String descripcion;
 
-    public Aliado(Point pos, int vida, int damage, int width, int height, float velocidadAtaque, Inteligencia IA) {
+    public Aliado(,Point pos, int vida, int damage, int width, int height, float velocidadAtaque, Inteligencia IA,String Descripcion) {
         super(pos, vida, damage, width, height, velocidadAtaque, IA);
+        image =new ImagenIcon[2];
+        descripcion=Descripcion;
+
     }
+
+    /**
+     * Devuelve la descripcion del Personaje Aliado
+     * @return String con personaje aliado
+     */
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+
 }
