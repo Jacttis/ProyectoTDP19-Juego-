ef8093b68d246e88c8c8f41833ef42cfa5fecaea
AxelK12 <51532945+AxelK12@users.noreply.github.com>
2019-10-14 17:55:33 -0300

    ReaperMan
    Clase Repaer Man y su Fabrica

---
+++ Juego/JuegoTDP19/src/Entidad/ReaperMan.java
@@ -0,0 +1,84 @@
+package Entidad;
+
+import FabricaDisparos.FabricaDisparoGolpeEnemigo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ReaperMan extends Enemigo {
+
+    public ReaperMan(int vida,int damage, float velocidadAtaque,double velocidad,int puntos,int oro){
+
+        super(vida,damage,velocidadAtaque,velocidad,puntos,oro);
+        fabricaDisparo=new FabricaDisparoGolpeEnemigo(this);
+
+        imagen[0] = new ImageIcon("Sprites/CharacterSprites/ReaperMan/walkingReaperMan2.gif");
+        imagen[1] = new ImageIcon("Sprites/CharacterSprites/ReaperMan/slashingReaperMan2.gif");
+        imagen[2] = new ImageIcon("Sprites/CharacterSprites/ReaperMan/dyngReaperMan2.gif");
+
+    }
+
+    /**
+     * Redefinicion de getEtiquetaVida mas general
+     *
+     * @return JLabel etiquetaVida
+     */
+    public JLabel getEtiquetaVida(){
+        if(this.etiquetaVida==null){
+            this.etiquetaVida=new JLabel();
+            this.etiquetaVida.setBounds(pos.x,pos.y+this.height,80,5);
+            this.etiquetaVida.setOpaque(true);
+            this.etiquetaVida.setBackground(Color.GREEN);
+        }
+
+        return etiquetaVida;
+    }
+
+    /**
+     * Redefinicion de actualizarEtiquetaVida mas general.
+     *
+     * @param n
+     */
+
+    public void actualizarEtiquetaVida(int n){
+
+        if(etiquetaVida!=null) {
+            etiquetaVida.setBounds(pos.x+(this.width-110), pos.y+(this.height-10) , (etiquetaVida.getWidth()-((n*80)/vidaTotal)), etiquetaVida.getHeight());
+
+            if (vidaActual < 70) {
+                if (vidaActual < 40)
+                    etiquetaVida.setBackground(Color.RED);
+                else etiquetaVida.setBackground(Color.YELLOW);
+            }
+        }
+    }
+
+    public Entidad clone(){
+        return new ReaperMan(vidaTotal,damage,velocidadAtaque,velocidad,puntos,oroPremio);
+    }
+    /**
+     *
+     * Implementacion del metodo abstracto atacar ubicado en Personaje.
+     * Si puede atacar genera un golpe mediante la fabrica de Disparos la cual lo insertara en el mapa.
+     *
+     */
+
+    public void atacar(){
+
+        if(puedeAtacar)
+            fabricaDisparo.generarDisparo();
+    }
+
+    /**
+     * Redefinicion del getHitbox mas general.
+     *
+     * Devuelve un rectangulo que reprensenta el hitbox del personaje.
+     *
+     * @return Rectangle
+     */
+
+    public Rectangle getHitBox(){
+        return new Rectangle(pos.x+40,pos.y,width-30,height-70);
+    }
+
+}
---
+++ Juego/JuegoTDP19/src/FabricaEnemigo/FabricaEnemigoReaperMan.java
@@ -0,0 +1,27 @@
+package FabricaEnemigo;
+
+import Entidad.Enemigo;
+import Entidad.ReaperMan;
+
+public class FabricaEnemigoReaperMan extends FabricaEnemigo {
+
+    protected Enemigo reaperManNormal,reaperManBoosted;
+    public static FabricaEnemigoReaperMan instance=null;
+
+    private FabricaEnemigoReaperMan(){
+        reaperManNormal=new ReaperMan(175,50,1500,2,150,2);
+        reaperManBoosted=new ReaperMan(175,50,1500,2,150,2);
+    }
+
+
+    public static FabricaEnemigoReaperMan getFabricaReaperMan(){
+        if(instance==null){
+            instance=new FabricaEnemigoReaperMan();
+        }
+        return instance;
+    }
+
+    public Enemigo crearEnemigo(){return (Enemigo) reaperManNormal.clone();}
+    public Enemigo crearEnemigoBoosted(){return (Enemigo) reaperManBoosted.clone();}
+
+}
