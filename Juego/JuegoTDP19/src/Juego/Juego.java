7e4412ead1f908b31a19aa01c68988e8ed8fee05
Jacttis <48808328+Jacttis@users.noreply.github.com>
2019-11-08 19:35:30 -0300

    Ganar y perder

--- Juego/JuegoTDP19/src/Juego/Juego.java
+++ Juego/JuegoTDP19/src/Juego/Juego.java
@@ -5,10 +5,9 @@ import Entidad.Aliados.Aliado;
 import FabricaEnemigo.FabricaEnemigoGolemHielo;
 import FabricaEnemigo.FabricaEnemigoGolemTierra;
 import GUI.GUI;
-
 import Tienda.*;
-
-
+import Menu.Menu;
+import javax.swing.*;
 import java.awt.*;
 import java.util.LinkedList;
 
@@ -75,8 +74,25 @@ public class Juego {
 	}
 
 
-	public void gameOver(){
+	public void gameOver(JLabel gameOver){  // Juego
 		instance=null;
+
+		gameOver.setBounds(850,80,3000,400);
+		gameOver.setForeground(Color.red);
+		Mapa.getMapa().getGui().add(gameOver);
+		gameOver.setFont(new Font("Times New Roman", 4,100));
+		try{
+			Thread.sleep(2000);
+		}
+		catch (InterruptedException e){
+			e.printStackTrace();
+		}
+		Mapa.getMapa().getGui().getTiempo().terminate();
+		Mapa.getMapa().getGui().dispose();
+		Tienda.getTienda().gameOver();
+		Mapa.getMapa().gameOver();
+		Menu.getMenu();
+
 	}
 	
 	
